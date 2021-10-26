8d4c0e766ca9d6eb4320d6a7ba79c0f329d7f920
zopaj63 <zoran.pajnic@gmail.com>
2021-10-26 18:54:52 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/zopaj63/nasa-api"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          My GitHub Repository
         </a>
       </header>
     </div>
