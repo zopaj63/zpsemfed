c188809868900ac6de0a1cfbe9ebfc3b97bf4277
zopaj63 <zoran.pajnic@gmail.com>
2021-10-26 19:53:11 +0200

    add Tutorial React.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
+import React, { Component } from 'react';
+import Messages from './components/Messages';
+import Input from './components/Input';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+function randomName() {
+  const adjectives = [
+    "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
+    "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
+    "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
+    "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"
+  ];
+  const nouns = [
+    "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+    "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
+    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+    "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
+    "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
+    "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
+    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+    "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"
+  ];
 
-        <a
-          className="App-link"
-          href="https://github.com/zopaj63/nasa-api"
-          target="_blank"
-          rel="noreferrer"
-        >
-          My GitHub Repository
-        </a>
-      </header>
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return adjective + noun;
+}
+
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
+class App extends Component {
+  state = {
+    messages: [],
+    member: {
+      username: randomName(),
+      color: randomColor(),
+    }
+  }
+
+  constructor() {
+    super();
+    this.drone = new window.Scaledrone('VIgNNXjmYel6Vmhu', {
+      data: this.state.member
+    });
+    this.drone.on('open', error => {
+      if (error) {
+        return console.error(error);
+      }
+      const member = { ...this.state.member };
+      member.id = this.drone.clientId;
+      this.setState({ member });
+    });
+    const room = this.drone.subscribe('observable-room');
+    room.on('data', (data, member) => {
+      const messages = this.state.messages;
+      messages.push({ member, text: data });
+      this.setState({ messages });
+    });
+  }
+
+  render() {
+    return (<div className="App" >
+      <div className="App-header" >
+        <h1 > My Chat App </h1> </div>
+      <Messages messages={this.state.messages}
+        currentMember={this.state.member} />
+      <Input onSendMessage={this.onSendMessage} />
     </div>
-  );
+    );
+  }
+
+  onSendMessage = (message) => {
+    this.drone.publish({
+      room: "observable-room",
+      message
+    });
+  }
+
 }
 
 export default App;
